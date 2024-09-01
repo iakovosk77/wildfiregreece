0144a1e17df1e21ca4b0a5d244b0388f4172859d
iakovosk77 <iakovosgames@gmail.com>
2024-09-01 21:04:13 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,17 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.image-slider .slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000); 
+
+showSlide(currentSlide);
